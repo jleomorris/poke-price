d5cb96865193e6dca946f2ac6487cd2cf727f205
jleomorris <james-morris2010@hotmail.co.uk>
2021-09-19 17:12:38 +0100

    Update homepage styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
+// Next
 import Head from 'next/head';
 import Image from 'next/image';
 import homeStyles from '../styles/Home.module.css';
+// Components
+import RandomCard from '../components/RandomCard';
+import CardSearch from '../components/CardSearch';
 
 const API_URL: string = 'https://api.pokemontcg.io/v2/cards';
 
@@ -8,7 +12,7 @@ const Home: React.FC = ({ randomCard }) => {
   console.log('home.randomCard', randomCard);
 
   return (
-    <main className='w-screen min-h-screen flex justify-center items-center bg-gray-100'>
+    <main className='w-screen min-h-screen flex flex-wrap justify-center bg-gray-100'>
       <Head>
         <title>Pokemon TCG Price Guide</title>
         <meta
@@ -18,18 +22,21 @@ const Home: React.FC = ({ randomCard }) => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <section className='md:w-5/12 border border-blue-500 min-h-screen h-screen relative'>
+      <section className='xl:w-5/12 border border-blue-500 w-full min-h-screen relative'>
         <div className='image-overlay h-full w-full bg-gradient-to-r from-black absolute top-0 left-0'></div>
-        <h1 className='text-8xl text-white font-bold absolute top-10 left-10 z-0'>
-          Pokemon TCG <span className='text-purple-500'>Price Guide</span>
+        <h1 className='text-6xl md:text-8xl w-2/3 xl:w-3/4 text-white font-bold absolute top-10 left-10 z-0'>
+          Pokemon TCG <span className='text-blue-400'>Price Guide</span>
         </h1>
         <img
-          className='object-cover h-screen'
+          className='object-cover h-full'
           src='https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fmedia.japanpowered.com%2Fimages%2Fpokemon-trading-cards.jpg&f=1&nofb=1'
           alt='home background'
         />
       </section>
-      <section className='p-20 md:w-7/12 border border-blue-500 min-h-screen flex flex-col justify-center items-center'></section>
+      <section className='p-20 xl:w-7/12 border border-blue-500 min-h-screen flex flex-col justify-center items-center'>
+        <RandomCard randomCard={randomCard} />
+        <CardSearch />
+      </section>
     </main>
   );
 };

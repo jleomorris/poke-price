cbc150bec51de6188acd4008f913768b0f1b90a9
jleomorris <james-morris2010@hotmail.co.uk>
2021-09-20 02:03:57 +0100

    Add logic for different card types to RandomCard

--- components/RandomCard/index.tsx
+++ components/RandomCard/index.tsx
@@ -1,83 +1,170 @@
 import { useState, useEffect } from 'react';
+import Image from 'next/image';
+
 const RandomCard = ({ randomCard }) => {
+  const [isNormal, setIsNormal] = useState<boolean>(false);
   const [isHoloFoil, setIsHoloFoil] = useState<boolean>(false);
   const [isReverseHoloFoil, setIsReverseHoloFoil] = useState<boolean>(false);
+  const [selectedType, setSelectedType] = useState<string>('');
 
   useEffect(() => {
-    if (randomCard.tcgplayer.prices.holofoil) setIsHoloFoil(true);
-    if (randomCard.tcgplayer.prices.reverseHolofoil) setIsReverseHoloFoil(true);
+    setCardTypes();
   }, []);
+
+  // Set Card Types
+  const setCardTypes = (): void => {
+    if (randomCard.tcgplayer.prices.reverseHolofoil) {
+      setIsReverseHoloFoil(true);
+      setSelectedType('reverse holo foil');
+    }
+    if (randomCard.tcgplayer.prices.holofoil) {
+      setIsHoloFoil(true);
+      setSelectedType('holo foil');
+    }
+    if (randomCard.tcgplayer.prices.normal) {
+      setIsNormal(true);
+      setSelectedType('normal');
+    }
+  };
 
   return (
     <>
       <h2 className='text-7xl tracking-tighter mb-10 text-white'>
         Random Card
       </h2>
-      <div className=''>
-        <div className='tabs ml-3 flex'>
-          {isHoloFoil && (
-            <p className='mr-2 px-4 py-2 bg-white bg-opacity-70 rounded-t-xl'>
-              Holofoil
-            </p>
-          )}
-          {isReverseHoloFoil && (
-            <p className='mr-2 px-4 py-2 bg-white bg-opacity-70 rounded-t-xl'>
-              Reverse Holofoil
-            </p>
-          )}
-        </div>
-        <div className='h-96 flex bg-white bg-opacity-70 shadow-lg rounded-xl'>
-          <img
+      <div className='tabs ml-3 flex'>
+        {isNormal && (
+          <button
+            className='mr-2 px-4 py-2 bg-white bg-opacity-70 rounded-t-xl'
+            onClick={() => setSelectedType('normal')}
+          >
+            Normal
+          </button>
+        )}
+        {isHoloFoil && (
+          <button
+            className='mr-2 px-4 py-2 bg-white bg-opacity-70 rounded-t-xl'
+            onClick={() => setSelectedType('holo foil')}
+          >
+            Holofoil
+          </button>
+        )}
+        {isReverseHoloFoil && (
+          <button
+            className='mr-2 px-4 py-2 bg-white bg-opacity-70 rounded-t-xl'
+            onClick={() => setSelectedType('reverse holo foil')}
+          >
+            Reverse Holofoil
+          </button>
+        )}
+      </div>
+      <div className='h-96 flex shadow-lg rounded-xl'>
+        {/* <img
+          src={randomCard.images.large}
+          className='h-full'
+          alt='random card'
+        /> */}
+        <div className='relative'>
+          <Image
             src={randomCard.images.large}
-            className='h-full'
+            // className='h-full'
             alt='random card'
+            layout='fixed'
+            height={400}
+            width={295}
+            // objectFit='contain'
           />
-          <div className='p-3'>
-            <p className='font-bold text-4xl uppercase tracking-widest'>
-              {randomCard.name}
+        </div>
+        <div className='p-3 bg-white bg-opacity-70'>
+          <p className='font-bold text-4xl uppercase tracking-widest'>
+            {randomCard.name}
+          </p>
+          <p className='text-xl uppercase tracking-widest'>
+            {randomCard.set.name}
+          </p>
+          <p className='text-md uppercase tracking-widest'>
+            Release date: {randomCard.set.releaseDate}
+          </p>
+          <p className='text-md uppercase tracking-widest'>
+            No. printed: {randomCard.set.printedTotal}
+          </p>
+          <div className='inline-block shadow-inner p-4 mt-5 bg-white bg-opacity-80 m-1  rounded-xl'>
+            <p className='font-bold text-xl'>
+              Market Price (
+              {(selectedType === 'holo foil' && 'holofoil') ||
+                (selectedType === 'reverse holo foil' && 'reverse holo foil') ||
+                (selectedType === 'normal' && 'normal')}
+              )
             </p>
-            <p className='text-xl uppercase tracking-widest'>
-              {randomCard.set.name}
+            <p className='font-bold text-7xl'>
+              $
+              {(selectedType === 'holo foil' &&
+                randomCard.tcgplayer?.prices?.holofoil?.market) ||
+                (selectedType === 'reverse holo foil' &&
+                  randomCard.tcgplayer?.prices?.reverseHolofoil?.market) ||
+                (selectedType === 'normal' &&
+                  randomCard.tcgplayer?.prices?.normal?.market)}
             </p>
-            <p className='text-md uppercase tracking-widest'>
-              Release date: {randomCard.set.releaseDate}
-            </p>
-            <p className='text-md uppercase tracking-widest'>
-              No. printed: {randomCard.set.printedTotal}
-            </p>
-            <div className='inline-block shadow-inner p-4 mt-5 bg-white bg-opacity-80 m-1  rounded-xl'>
-              <p className='font-bold text-xl'>Market Price (Holofoil) </p>
-              <p className='font-bold text-7xl'>
-                ${randomCard.tcgplayer?.prices?.holofoil?.market}
-              </p>
-              {/* <div className='shadow-inner p-4 bg-white bg-opacity-80 m-1 flex flex-col items-center rounded-r-xl'>
-              <p className='font-bold text-xl'>Updated at </p>
-              <p className='font-bold text-6xl'>
-                {randomCard.tcgplayer?.updatedAt}
-              </p>
-            </div> */}
-            </div>
           </div>
-          <div className=' rounded-r-xl overflow-hidden'>
-            <div className='flex flex-col h-full'>
-              <div className='shadow-inner p-4 bg-white bg-opacity-80 h-1/2 flex flex-col items-center'>
-                <p className='font-bold text-xl'>Low Price (Holofoil) </p>
-                <p className='font-bold text-6xl text-red-500 '>
-                  ${randomCard.tcgplayer?.prices?.holofoil?.low}
-                </p>
-              </div>
-              <div className='shadow-inner p-4 bg-white bg-opacity-80 h-1/2 flex flex-col items-center'>
-                <p className='font-bold text-xl'>Mid Price (Holofoil) </p>
-                <p className='font-bold text-6xl text-yellow-500'>
-                  ${randomCard.tcgplayer?.prices?.holofoil?.mid}
-                </p>
-              </div>
-              <div className='shadow-inner p-4 bg-white bg-opacity-80 h-1/2 flex flex-col items-center'>
-                <p className='font-bold text-xl'>High Price (Holofoil) </p>
-                <p className='font-bold text-6xl text-green-500'>
-                  ${randomCard.tcgplayer?.prices?.holofoil?.high}
-                </p>
-              </div>
+        </div>
+        <div className=' rounded-r-xl overflow-hidden'>
+          <div className='flex flex-col h-full'>
+            <div className='shadow-inner p-4 bg-white bg-opacity-80 h-1/2 flex flex-col items-center'>
+              <p className='font-bold text-xl'>
+                Low Price (
+                {(selectedType === 'holo foil' && 'holofoil') ||
+                  (selectedType === 'reverse holo foil' &&
+                    'reverse holo foil') ||
+                  (selectedType === 'normal' && 'normal')}
+                )
+              </p>
+              <p className='font-bold text-6xl text-red-500 '>
+                $
+                {(selectedType === 'holo foil' &&
+                  randomCard.tcgplayer?.prices?.holofoil?.low) ||
+                  (selectedType === 'reverse holo foil' &&
+                    randomCard.tcgplayer?.prices?.reverseHolofoil?.low) ||
+                  (selectedType === 'normal' &&
+                    randomCard.tcgplayer?.prices?.normal?.low)}
+              </p>
+            </div>
+            <div className='shadow-inner p-4 bg-white bg-opacity-80 h-1/2 flex flex-col items-center'>
+              <p className='font-bold text-xl'>
+                Mid Price (
+                {(selectedType === 'holo foil' && 'holofoil') ||
+                  (selectedType === 'reverse holo foil' &&
+                    'reverse holo foil') ||
+                  (selectedType === 'normal' && 'normal')}
+                )
+              </p>
+              <p className='font-bold text-6xl text-yellow-500'>
+                $
+                {(selectedType === 'holo foil' &&
+                  randomCard.tcgplayer?.prices?.holofoil?.mid) ||
+                  (selectedType === 'reverse holo foil' &&
+                    randomCard.tcgplayer?.prices?.reverseHolofoil?.mid) ||
+                  (selectedType === 'normal' &&
+                    randomCard.tcgplayer?.prices?.normal?.mid)}
+              </p>
+            </div>
+            <div className='shadow-inner p-4 bg-white bg-opacity-80 h-1/2 flex flex-col items-center'>
+              <p className='font-bold text-xl'>
+                High Price (
+                {(selectedType === 'holo foil' && 'holofoil') ||
+                  (selectedType === 'reverse holo foil' &&
+                    'reverse holo foil') ||
+                  (selectedType === 'normal' && 'normal')}
+                )
+              </p>
+              <p className='font-bold text-6xl text-green-500'>
+                $
+                {(selectedType === 'holo foil' &&
+                  randomCard.tcgplayer?.prices?.holofoil?.high) ||
+                  (selectedType === 'reverse holo foil' &&
+                    randomCard.tcgplayer?.prices?.reverseHolofoil?.high) ||
+                  (selectedType === 'normal' &&
+                    randomCard.tcgplayer?.prices?.normal?.high)}
+              </p>
             </div>
           </div>
         </div>

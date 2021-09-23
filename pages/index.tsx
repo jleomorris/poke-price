fa6ee72069f3bcebf306a9e7cc87f8bfe2ec4e02
jleomorris <james-morris2010@hotmail.co.uk>
2021-09-23 01:48:27 +0100

    Update layout of homepage to match new design

--- pages/index.tsx
+++ pages/index.tsx
@@ -78,23 +78,31 @@ const Home: React.FC = ({ randomCard }) => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <section className='p-5 sm:p-20 w-full xl:min-h-screen relative flex flex-col justify-start items-start'>
-        <img
-          className='object-cover h-full w-full absolute top-0 left-0 '
-          src='https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fmedia.japanpowered.com%2Fimages%2Fpokemon-trading-cards.jpg&f=1&nofb=1'
-          alt='home background'
-        />
-        <div className='image-overlay h-full w-full bg-gradient-to-r from-black absolute top-0 left-0'></div>
-        <div className='w-full xl:w-9/12 border border-blue-500 relative'>
-          <h1 className='text-6xl md:text-9xl mb-10 xl:w-2/3 text-white font-bold relative'>
-            Pokemon TCG <span className='text-blue-400'>Price Guide</span>
+      <section className='w-full xl:min-h-screen relative flex justify-start items-start'>
+        <div className='p-20 w-full xl:w-7/12 border min-h-screen border-blue-500 relative'>
+          <h1 className='text-6xl text-black md:text-8xl mb-10 xl:w-3/4 font-bold relative'>
+            Quickly find a card price with{' '}
+            <span className='text-blue-400'>PokePrice</span>
           </h1>
-          <div className='w-2/3 lg:w-1/3'>
+          <ul className='ml-8 list-disc text-3xl xl:w-1/2'>
+            <li>Card prices from TCGPlayer </li>
+            <li>Find cards by name, type, release date, legality, and more</li>
+            <li>Developed using PokemonTCG API</li>
+          </ul>
+          <div className='card-search-container absolute z-10 w-2/3 lg:w-1/2'>
             <CardSearch searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
           </div>
         </div>
+        <div className='background-img relative xl:w-5/12 min-h-screen border border-red'>
+          <img
+            className='object-cover h-full w-full absolute top-0 left-0 '
+            src='https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fmedia.japanpowered.com%2Fimages%2Fpokemon-trading-cards.jpg&f=1&nofb=1'
+            alt='home background'
+          />
+          <div className='image-overlay h-full w-full bg-gradient-to-t from-white absolute top-0 left-0'></div>
+        </div>
       </section>
-      <section className='w-full p-10 xl:w-3/12 xl:min-h-screen text-center absolute top-0 right-0 bg-black bg-opacity-70'>
+      <section className='features hidden w-full p-10 xl:w-3/12 xl:min-h-screen text-center absolute top-0 right-0 bg-black bg-opacity-70'>
         <div className='usp my-10'>
           <img
             src='https://mp-assets.tcgplayer.com/img/TCGplayer-logo-primary@2x.png'

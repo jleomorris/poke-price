b03268c16640221d2cb1c26d2b8403b31dc22740
jleomorris <james-morris2010@hotmail.co.uk>
2021-09-23 01:48:41 +0100

    Update design of CardSearch

--- components/CardSearch/index.tsx
+++ components/CardSearch/index.tsx
@@ -18,18 +18,42 @@ const CardSearch: React.FC<Iprops> = ({ searchTerm, setSearchTerm }) => {
   };
 
   return (
-    <div className='my-10 w-full bg-white bg-opacity-80 py-5 px-10 rounded-xl relative'>
-      <h2 className='text-3xl font-thin'>Search For Card</h2>
+    <div className='my-10 w-full bg-black bg-opacity-90 py-10 px-14 rounded-3xl relative'>
+      <h2 className='text-3xl text-white text-center mb-3 font-thin'>Search</h2>
       {/* <form onSubmit={(e) => submitHandler} action='/'> */}
-      <input
-        type='text'
-        className='w-full my-3 p-3 rounded-xl shadow-inner'
-        placeholder='Enter card name or number'
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
+      <div className='relative'>
+        <div className='magnification-icon-container absolute rounded-full p-2 bg-gray-200'>
+          <button onClick={submitHandler}>
+            <svg
+              width='23'
+              height='19'
+              viewBox='0 0 16 15'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <g transform='translate(.5)' stroke='#242424' fill='none'>
+                <circle cx='5.5' cy='5.5' r='4.75'></circle>
+                <rect
+                  transform='rotate(-45 11.357 11.457)'
+                  x='11.107'
+                  y='8.207'
+                  width='1'
+                  height='6.5'
+                  rx='.5'
+                ></rect>
+              </g>
+            </svg>
+          </button>
+        </div>
+        <input
+          type='text'
+          className='w-full my-3 p-3 rounded-full pl-5 shadow-inner'
+          placeholder='Enter card name or number'
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </div>
       <button
-        className='py-3 px-5 bg-white shadow-md rounded-xl'
+        className='py-3 px-8 bg-white shadow-md rounded-full mx-auto block'
         onClick={submitHandler}
       >
         Submit

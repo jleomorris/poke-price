1d479240062c70b59a05dcbadbefe3f6616d0a08
jleomorris <james-morris2010@hotmail.co.uk>
2021-10-09 03:14:14 +0100

    Add joinClassNames util function

--- utils/index.tsx
+++ utils/index.tsx
@@ -7,3 +7,9 @@ export const removeSpecialChars = (str: string): string =>
 
 export const convertToDecimals = (value: number, decimals = 2) =>
   (Math.round(value * 100) / 100).toFixed(decimals);
+
+export const joinClassNames = (
+  ...classes: (false | null | undefined | string)[]
+): string => {
+  return classes.filter(Boolean).join(' ');
+};
